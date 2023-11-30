40b39758d9ef324e3725de4944ebcf015c6db286
wayungi <fwayungi.com>
2023-11-30 14:43:55 +0300

    FIX: image carousel buggy auto scroll

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -8,17 +8,14 @@ const ImageSlider = () => {
 
     useEffect(() => {
         setTimeout(() => setIndex((prevIndex) => prevIndex ===  CarouselImages.length - 1 ? 0 : prevIndex + 1), delay);
-        return () => {};
+        // return () => {};
     }, [index])
 
-
-    const carousel = CarouselImages.map((image) => <div 
-        className="slide-show" style={{ transform: `translate(${-index * 100}%)` }}
-        >
+    const carousel = CarouselImages.map((image) => <div className="slide-show" style={{ transform: `translate(${-index * 100}%)` }}>
         <img src={image} alt="carousel image" className="slide"/>
-        </div>
-    );
-    const slideShowDots = CarouselImages.map((_, indx) => <div key={indx} className={`slide-show-dot ${index === indx ? "active" : ""}`}></div>);
+    </div>);
+
+    const slideShowDots = CarouselImages.map((_, indx) => <div key={indx} className={ `slide-show-dot ${index === indx ? "active" : ""}`}></div>);
 
     return (
         <section className="image-slider">
@@ -30,4 +27,4 @@ const ImageSlider = () => {
     );
 }
 
-export default ImageSlider
+export default ImageSlider;

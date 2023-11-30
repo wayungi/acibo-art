3c22e255fe90318185942c83f305f30427b30944
wayungi <fwayungi.com>
2023-11-30 20:02:09 +0300

    INSTALL; react-icons

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { GrClose } from "react-icons/gr";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const NavBar = () => {
+  return (
+    <nav>
+        <GrClose />
+        <GiHamburgerMenu />
+
+    </nav>
+  )
+}
+
+export default NavBar

c4288dd89eb128899bc501be8b0a6902ff11eed8
wayungi <fwayungi.com>
2023-11-30 09:45:53 +0300

    ADD: background image for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import { CarouselImages } from "../util/carouselImages";
 
 const Home = () => {
+
+  const carousel = CarouselImages.map((image) => <img src={image} alt="carousel image" />)
   return (
-    <div>Home</div>
+  <div className="hpome-page">{carousel}</div>
   )
 }
 
---
+++ src/util/carouselImages.tsx
@@ -0,0 +1,6 @@
+import art from '../assets/large-art.jpg';
+import bicycle from '../assets/large-bicycle.jpg';
+import graffiti from '../assets/large-graffiti.jpg'
+
+export const CarouselImages = [art, bicycle, graffiti];
+

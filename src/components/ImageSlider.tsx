d8258fb69591abb9fb390b3592bbf751af58890f
wayungi <fwayungi.com>
2023-11-30 11:51:39 +0300

    ADD: auto image change

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from "react";
 import { CarouselImages } from "../util/carouselImages";
 
 
 const ImageSlider = () => {
-    const carousel = CarouselImages.map((image) => <div className="bg-frame"><img src={image} alt="carousel image" /></div>);
+    const delay = 3000;
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => setIndex((prevIndex) => prevIndex ===  CarouselImages.length - 1 ? 0 : prevIndex + 1), delay);
+        // return () => {};
+    }, [index])
+
+
+    const carousel = CarouselImages.map((image) => <div 
+        className="slide-show" style={{ transform: `translate(${-index * 100}%)` }}
+        >
+        <img src={image} alt="carousel image" className="slide"/>
+        </div>
+    );
+    const slideShowDots = CarouselImages.map((_, index) => <div key={index} className="slide-show-dot"></div>);
+
     return (
         <section className="image-slider">
             {carousel}
+            <div className="slide-show-dots">
+                {slideShowDots}
+            </div>
         </section>
     );
 }
